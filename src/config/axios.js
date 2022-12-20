41bac1238f4c2f90d96c7e7179fdeb34baee8353
ss1290 <57432666+ss1290@users.noreply.github.com>
2022-12-20 17:59:17 +0530

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://go-talk-backend-production.up.railway.app/",
-  
-//   baseURL: "http://localhost:8000",
-//   localdev
+  baseURL: "https://go-talk-backend-production.up.railway.app",
 });
 
 export default instance;
